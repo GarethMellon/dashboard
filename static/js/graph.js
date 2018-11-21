ddb13957895dd23d4106634ec5afa182591519a6
Gareth_Mellon <garethmellon@gmail.com>
2018-11-21 15:57:50 +0000

    add chart 1

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,7 +1,40 @@
 queue()
     .defer(d3.csv, "data/Salaries.csv")
     .await(makeGraphs);
+
+function makeGraphs(error, salaryData) {
+    var ndx = crossfilter(salaryData);
+
+    showDisciplineSelector(ndx);
+    show_gender_balance(ndx);
     
-function makeGraphs (error, salaryData){
-    
+
+    dc.renderAll();
 };
+
+
+function showDisciplineSelector(ndx) {
+    var dim = ndx.dimension(dc.pluck('discipline'));
+    var group = dim.group()
+
+    dc.selectMenu('#discipline-selector')
+        .dimension(dim)
+        .group(group);
+};
+
+function show_gender_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck("sex"));
+    var group = dim.group();
+
+    dc.barChart("#gender-balance")
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(20);
+}
